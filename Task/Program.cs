25b6c228d565a97ef93d51f23fb44111cc125c8f
Alexandra2025 <turkova2025@gmail.com>
2022-10-18 09:20:18 +0800

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, флина которых меньше либо равна 3.
+//////////////////////////////////////////////////////////////
+// Ввод массива
+Console.WriteLine("Введите элементы массива через пробел");
+string? stroka = Console.ReadLine();
+string[] array1 = stroka!.Split(' ');
+
+// Метод преобразует массив в строку
+string Stroka(string[] arr)
+{
+  string? stroka1 = "";
+  foreach (var value in arr)
+  {
+    if (value.Length <= 3)
+    {
+      stroka1 = stroka1 + value + " ";
+    }
+  }
+  stroka1 = stroka1.Remove(stroka1.Length - 1);
+  return stroka1;
+}
+
+
+string stroka2 = Stroka(array1);
+string[] array2 = stroka2!.Split(' ');
+var s = string.Join("\", \"", array2);
+Console.Write("[\"");
+Console.Write(s);
+Console.Write("\"]");
+Console.ReadKey();
